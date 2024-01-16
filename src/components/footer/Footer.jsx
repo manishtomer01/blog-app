7979bb4885598e4f6218e79f46c961fa59433b4f
Manish Tomer <mt0578902@gmail.com>
2024-01-16 20:51:29 +0530

    fix bugs

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,9 +3,9 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>lamadev</div>
+      <div className={styles.logo}>BlogApp</div>
       <div className={styles.text}>
-        Lama creative thoughts agency © All rights reserved.
+       © All rights reserved.
       </div>
     </div>
   );
